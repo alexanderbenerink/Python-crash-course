0f3d56bb330089254558290a8f4cebd09c61f4ef
jellegro <jellegro@hotmail.nl>
2024-08-26 19:08:57 +0200

    Decorator update

--- endresults_jelle/decorators.py
+++ endresults_jelle/decorators.py
@@ -5,8 +5,9 @@ def time_function(func):
     @wraps(func)
     def wrapper(*args, **kwds):
         start = time.perf_counter()
-        func(*args, **kwds)
+        function = func(*args, **kwds)
         end = time.perf_counter()
 
         print(f"Function {func.__name__} took: {'%.4f' % (end - start)} seconds.")
+        return function
     return wrapper
