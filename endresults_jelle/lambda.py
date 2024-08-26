cd3c8505ed1f93bf03c62959f14b0523050629fa
jellegro <jellegro@hotmail.nl>
2024-08-26 15:21:19 +0200

    Lambda assignment - update
    Added second lambda function

--- endresults_jelle/lambda.py
+++ endresults_jelle/lambda.py
@@ -1,9 +1,24 @@
-if __name__=="__main__":
-    numbers: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+def lambda_squared(numbers: list) -> list:
     square = lambda x: x ** 2
     squared = list(map(square, numbers))
 
+    return squared
+
+def lambda_even(numbers: list) -> list:
+    even = lambda x: x % 2 == 0
+    even_list = list(filter(even, numbers))
+
+    return even_list
+
+if __name__=="__main__":
+    numbers: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
     print(numbers)
-    print("Squared: ")
-    print(squared) 
+    print('Squared: ')
+    print(lambda_squared(numbers))
+
+    print('\n')
+    
+    print(numbers)
+    print('Even: ')
+    print(lambda_even(numbers))
+
